c09f39aff8dde8a45921dcf5228052e3cb55b267
Marlon <marlonwen2@outlook.com>
2024-09-16 21:34:33 +0100

    Add files via upload

---
+++ testweb/urls.py
@@ -0,0 +1,42 @@
+"""
+URL configuration for testweb project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from . import views
+'''
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.default_page, name='default_page'),
+    path('register/', views.register, name='register'),
+    path('home/<str:username>/', views.home_view, name='home'),
+    path('home/', views.redirect_home, name='home_redirect'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('home.urls')),
+]
+'''
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('home/<str:username>/', views.home_view, name='home'),
+    path('home/', views.redirect_home, name='home_redirect'),
+    path('', views.default_page, name='default_page'),  # Ensure this is placed last or handled appropriately
+    path('', include('home.urls')),  # Ensure this does not conflict with other patterns
+]
