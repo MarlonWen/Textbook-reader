62f810340fd24e4139509666fb4c7aff8ea3c20a
Marlon <marlonwen2@outlook.com>
2024-09-24 13:45:23 +0100

    Update settings.py

--- testweb/settings.py
+++ testweb/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 ALLOWED_HOSTS = ['textbook-reader.onrender.com']
