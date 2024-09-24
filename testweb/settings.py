aa62a0f97e9f6ef8b9e5b5b997357f91546edd63
Marlon <marlonwen2@outlook.com>
2024-09-24 13:28:36 +0100

    Update settings.py

--- testweb/settings.py
+++ testweb/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q4aw^!i-_%0zl1m@x68s32%4xrf7qk$0f4*=b@wugel8kba^%-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
